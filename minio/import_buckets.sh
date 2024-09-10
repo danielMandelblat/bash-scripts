edfbc5c88139c51a20a8c6bb7fdb854278747d0e
Daniel Mandelblat <50299485+danielMandelblat@users.noreply.github.com>
2024-09-10 13:55:18 +0300

    Update import_buckets.sh

--- minio/import_buckets.sh
+++ minio/import_buckets.sh
@@ -3,23 +3,36 @@
 user="minio"
 pass="minio"
 
-# Create new alias
-mc alias set local_minio http://127.0.0.1:9000 $user $pass
-
 # Set your MinIO alias for the local MinIO instance
 LOCAL_ALIAS="local_minio"   # The alias for your local MinIO
 
 # Define the path to the local folder where buckets are exported
-LOCAL_FOLDER="/path/to/exported/buckets"  # Replace with the actual path to your local folder
+LOCAL_FOLDER="/tmp/buckets"  # Replace with the actual path to your local folder
 
-# Define the list of buckets to mirror
-buckets=("bucket1" "bucket2" "bucket3")  # Add your bucket names here
+# Create new alias
+mc alias set $LOCAL_ALIAS http://127.0.0.1:9000 $user $pass
 
 # Loop through each bucket and mirror to local MinIO
-for bucket in "${buckets[@]}"
+for bucket in $(ls $LOCAL_FOLDER)
 do
+
+  echo "Checking if bucket $bucket exists on $LOCAL_ALIAS..."
+
+  # Check if the bucket exists
+  mc ls "$LOCAL_ALIAS/$bucket" > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Bucket $bucket does not exist. Creating it..."
+    mc mb "$LOCAL_ALIAS/$bucket"
+    if [ $? -ne 0 ]; then
+      echo "Error creating bucket $bucket. Skipping..."
+      continue
+    fi
+  else
+    echo "Bucket $bucket already exists."
+  fi
+
   echo "Mirroring $bucket from $LOCAL_FOLDER to $LOCAL_ALIAS..."
-  mc mirror --overwrite --remove "$LOCAL_FOLDER/$bucket" "$LOCAL_ALIAS/$bucket"
+  mc mirror --overwrite --insecure --remove "$LOCAL_FOLDER/$bucket" "$LOCAL_ALIAS/$bucket"
   
   if [ $? -eq 0 ]; then
     echo "Successfully mirrored $bucket."
