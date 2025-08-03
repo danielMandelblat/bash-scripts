6b626afb4ffe107fe1da46c74b4df521f53f0e33
Daniel Mandelblat <50299485+danielMandelblat@users.noreply.github.com>
2025-08-03 16:25:31 +0300

    Create clean-unclean-ipaddress.sh

---
+++ spectro/clean-unclean-ipaddress.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+###############################################################################
+# Script Name: cleanup-unused-ipaddresses.sh
+# Description: 
+#   This script identifies and prints commands to delete unused IP addresses 
+#   from Metal3 IPAM in a Kubernetes cluster. It compares the list of allocated
+#   IPs against the actual node IPs and skips the VIP used by the API server.
+#
+# Author: Daniel Mandelblat
+# Date: 2025-08-03
+###############################################################################
+
+# Get the API server's VIP (without port)
+vip="$(kubectl get kubeadmconfig -A -o json \
+      | jq -r '.items[0].spec.joinConfiguration.discovery.bootstrapToken.apiServerEndpoint' \
+      | awk -F ":" '{print $1}')"
+
+# Get all IP addresses from IPAM
+readarray -t ipaddress < <(
+    kubectl get ipaddresses.ipam.metal3.io -A -o json \
+    | jq -r '.items[].spec.address'
+)
+
+# Get all node IPs from Calico annotations (without CIDR)
+nodes_ips=($(
+    kubectl get node -o json \
+    | jq -r '.items[].metadata.annotations["projectcalico.org/IPv4Address"]' \
+    | awk -F "/" '{print $1}'
+))
+
+# Loop through IP addresses from IPAM
+for ip in "${ipaddress[@]}"; do
+    # Skip the API server VIP
+    if [[ "$vip" == "$ip" ]]; then
+        continue
+    fi
+
+    exist="false"
+    for node_ip in "${nodes_ips[@]}"; do
+        if [[ "$ip" == "$node_ip" ]]; then
+            exist="true"
+            break
+        fi
+    done
+
+    if [[ "$exist" == "false" ]]; then
+        # Fetch full IP object details
+        ipaddress_object="$(kubectl get ipaddresses.ipam.metal3.io -A -o json \
+            | jq -r --arg ip "$ip" '.items[] | select(.spec.address == $ip)')"
+        ipaddress_name="$(echo "$ipaddress_object" | jq -r '.metadata.name')"
+        ipaddress_namespace="$(echo "$ipaddress_object" | jq -r '.metadata.namespace')"
+
+        # Print the delete command
+        echo "kubectl delete ipaddresses.ipam.metal3.io -n $ipaddress_namespace $ipaddress_name"
+    fi
+done
